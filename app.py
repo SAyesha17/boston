e2f9bc9849b2f9a83bb3898cf828d60f5c7232bc
SAyesha <shaikayeshask22@gmail.com>
2023-05-13 00:25:04 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+scaler = pickle.load(open('scaling.pkl','rb'))
+reg_model = pickle.load(open('regmodel.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    #return 'Hello'
+    return render_template('home.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_data = scaler.transform(np.array(data).reshape(1,-1))
+    output = reg_model.predict(final_data)[0]
+
+    return render_template('home.html',prediction_text='House price is {}'.format(output))
+
+app.run(debug=True)
